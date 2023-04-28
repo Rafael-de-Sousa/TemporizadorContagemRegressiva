fe6011cf085aaeb74f4eda2dfcc6cb99926001a9
Rafael <fael.ra81@gmail.com>
2023-04-28 11:38:51 -0300

    Inicio da interface.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace TemporizadorContagemRegressiva
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNomeEvento = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblHoras = new System.Windows.Forms.Label();
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(74, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(571, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contagem Regressiva";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(93, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome do evento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(392, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(565, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hora";
+            // 
+            // txtNomeEvento
+            // 
+            this.txtNomeEvento.Font = new System.Drawing.Font("Snap ITC", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNomeEvento.Location = new System.Drawing.Point(44, 132);
+            this.txtNomeEvento.Name = "txtNomeEvento";
+            this.txtNomeEvento.Size = new System.Drawing.Size(290, 24);
+            this.txtNomeEvento.TabIndex = 4;
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Snap ITC", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtData.Location = new System.Drawing.Point(365, 132);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(136, 24);
+            this.txtData.TabIndex = 5;
+            this.txtData.Text = "00/00/0000";
+            // 
+            // txtHora
+            // 
+            this.txtHora.Font = new System.Drawing.Font("Snap ITC", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtHora.Location = new System.Drawing.Point(534, 132);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(136, 24);
+            this.txtHora.TabIndex = 6;
+            this.txtHora.Text = "00:00:00";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnIniciar.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnIniciar.Location = new System.Drawing.Point(232, 204);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(254, 65);
+            this.btnIniciar.TabIndex = 7;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(44, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 60);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Faltam";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.Location = new System.Drawing.Point(269, 316);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 17);
+            this.lblNome.TabIndex = 9;
+            this.lblNome.Text = "Dias";
+            this.lblNome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHoras
+            // 
+            this.lblHoras.AutoSize = true;
+            this.lblHoras.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHoras.Location = new System.Drawing.Point(365, 316);
+            this.lblHoras.Name = "lblHoras";
+            this.lblHoras.Size = new System.Drawing.Size(52, 17);
+            this.lblHoras.TabIndex = 10;
+            this.lblHoras.Text = "Horas";
+            this.lblHoras.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMinutos.Location = new System.Drawing.Point(477, 316);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(69, 17);
+            this.lblMinutos.TabIndex = 11;
+            this.lblMinutos.Text = "Minutos";
+            this.lblMinutos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSegundos.Location = new System.Drawing.Point(593, 316);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(77, 17);
+            this.lblSegundos.TabIndex = 12;
+            this.lblSegundos.Text = "Segundos";
+            this.lblSegundos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(716, 409);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.lblMinutos);
+            this.Controls.Add(this.lblHoras);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtNomeEvento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Temporizador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNomeEvento;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblHoras;
+        private System.Windows.Forms.Label lblMinutos;
+        private System.Windows.Forms.Label lblSegundos;
+    }
+}
+
